d7ac2810c987dc0c34fb1bb43830559c327808eb
MRKB <asdfgh7913@naver.com>
2024-10-01 21:23:35 +0900

    user

--- includeHtml.js
+++ includeHtml.js
@@ -1,3 +1,4 @@
+let isLoggedIn = false;
 const includeHTML = () => {
     const docs = document.querySelectorAll("*");
     docs.forEach((e) => {
@@ -14,12 +15,16 @@ const includeHTML = () => {
 }
 const activateNav = () => {
     const navList = document.querySelectorAll(".nav-button");
-    console.log(navList);
     navList.forEach(e => {
         const href = e.getAttribute("href").split("/");
         const location = window.location.pathname.split(("/"));
         if (href[href.length -1] === location[location.length -1]) e.classList.add("active");
     });
+    const topMenu = document.querySelector("#top");
+    if (isLoggedIn) topMenu.innerHTML = `
+        <a id="register-button" href="./user-info.html">회원정보</a>
+        <a id="login-button" href="./logout.html">로그아웃</a>
+    `;
 }
 window.addEventListener('DOMContentLoaded', () => {
     includeHTML();
