42d3db7382192669b8db69397392dcef02f90020
MRKB <64984647+MaronKB@users.noreply.github.com>
2024-10-11 10:15:28 +0900

    .gitignore added, community list loads from json

--- js/community.js
+++ js/community.js
@@ -1,3 +1,64 @@
+function CommunityItemData(jsonData) {
+    this.postNumber  = jsonData.com_post_number;
+    this.title = jsonData.com_title;
+    this.imageName = jsonData.com_image_name;
+    this.imagePath = jsonData.com_image_path;
+    this.videoName = jsonData.com_video_name;
+    this.videoPath = jsonData.com_video_path;
+    this.content = jsonData.com_content;
+    this.date = jsonData.com_post_date;
+    this.viewCount = jsonData.com_view_count;
+    this.user = jsonData.usr_number;
+    this.nickname = jsonData.uer_nickname;
+}
+
+const getCommunityList = () => {
+    fetch("../resources/temp-db/community.json")
+        .then(res => res.json())
+        .then(data => createCommunityList(data));
+}
+
+const createCommunityList = (data) => {
+    const dataList = data.map(e => {
+        return new CommunityItemData(e);
+    }).sort((a, b) => {
+        return a.postNumber - b.postNumber;
+    });
+
+    const domList = dataList.map(e => {
+        const postNumber = document.createElement("span");
+        postNumber.className = "community-item-number";
+        postNumber.innerHTML = e.postNumber;
+
+        const title = document.createElement("h5");
+        title.className = "community-item-title";
+        title.innerHTML = e.title;
+
+        const nickname = document.createElement("span");
+        nickname.className = "community-item-nickname";
+        nickname.innerHTML = e.nickname;
+
+        const date = document.createElement("span");
+        date.className = "community-item-date";
+        date.innerHTML = e.date;
+
+        const view = document.createElement("span");
+        view.className = "community-item-view";
+        view.innerHTML = e.viewCount;
+
+        const list = document.createElement("li");
+        list.className = "community-item";
+        list.append(postNumber, title, nickname, date, view);
+
+        return list;
+    });
+    const communityListContainer = document.getElementById("community-list");
+    communityListContainer.innerHTML = "";
+    communityListContainer.append(...domList);
+}
+
+document.addEventListener('DOMContentLoaded', getCommunityList);
+/*
 document.addEventListener('DOMContentLoaded', function() {
     const posts = JSON.parse(localStorage.getItem('posts')) || [];
     const postsPerPage = 10; // 한 페이지에 표시할 글 수
@@ -69,3 +130,4 @@ document.addEventListener('DOMContentLoaded', function() {
     // 초기 글 목록 업데이트
     updateCommunityList();
 });
+*/
