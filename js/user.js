cebf40e002640b8b16c7b5a0a9ce5df5d2b53deb
MRKB <64984647+MaronKB@users.noreply.github.com>
2024-10-14 17:05:37 +0900

    user restyle

--- js/user.js
+++ js/user.js
@@ -4,6 +4,31 @@ const graphAnimation = () => {
         graph.classList.remove("ready");
     })
 }
+const calcBMR = () => {
+    const age = document.querySelector("#age").value;
+    const height = document.querySelector("#height").value;
+    const weight = document.querySelector("#weight").value;
+
+    const bmr = 6647 + (1375 * weight) + (500 * height) - (676 * age);
+
+    const target = document.querySelector("#kal");
+    target.value = bmr / 100;
+}
+const calcDate = () => {
+    const startDate = new Date(document.querySelector("#start-date").value);
+    const today = new Date();
+    const diff = Math.abs(today.getTime() - startDate.getTime());
+    const days = Math.ceil(diff / (1000 * 60 * 60 * 24));
+
+    const target = document.querySelector("#date-diff");
+    target.innerHTML = String(days);
+}
 window.addEventListener('DOMContentLoaded', () => {
+    const inputs = document.querySelectorAll('.user-input > input');
+    inputs.forEach((input) => {
+        input.onchange = calcBMR;
+    })
+    calcDate();
+    calcBMR();
     setTimeout(graphAnimation, 300);
 });
