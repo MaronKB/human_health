da7a40b9079de8ea440ac4da5ed03384bdc22423
MRKB <64984647+MaronKB@users.noreply.github.com>
2024-10-15 17:31:51 +0900

    vital ing

--- js/vital.js
+++ js/vital.js
@@ -7,8 +7,8 @@ const comments = {
         5 : "많이 피곤하셨나요?<br/>오늘은 좋아하는 음식을 먹어요.",
         6 : "힘든 일이 있었나요?<br/>취미활동을 시작해보세요.",
         7 : "안 좋은 일이 있었나봐요.<br/>가족이나 친구들과 이야기해봐요.",
-        8 : "행복한 것들을 눈에 담고,<br/>당신을 사랑하는 목소리에 귀기울이세요.",
-        9 : "믿을 만한 사람들에게 고민을 털어놓아보세요.<br/>힘들어도 결코 포기하지 말아요.",
+        8 : "행복한 것들을 눈에 담고,<br/>좋은 말들에 귀기울이세요.",
+        9 : "고민을 털어놓아보세요.<br/>힘들어도 결코 포기하지 말아요.",
         10 : "정신과 상담을 권장합니다.<br/>즉시 휴식을 취하십시오."
     },
     sleep : {
@@ -20,7 +20,7 @@ const comments = {
         5 : "",
         6 : "",
         7 : "",
-        8 : "",
+        8 : "정상적인 수면시간입니다.",
         9 : "",
         10 : "",
         11 : "",
@@ -39,6 +39,48 @@ const comments = {
         24 : "",
     }
 }
+const stretchList = [
+    {
+        index: 0,
+        title: "neck",
+        url: "",
+    },
+    {
+        index: 1,
+        title: "shoulder",
+        url: "",
+    },
+    {
+        index: 2,
+        title: "chest",
+        url: "",
+    },
+    {
+        index: 3,
+        title: "wrist",
+        url: "",
+    },
+    {
+        index: 4,
+        title: "ankle",
+        url: "",
+    },
+    {
+        index: 5,
+        title: "leg",
+        url: "",
+    },
+    {
+        index: 6,
+        title: "waist",
+        url: "",
+    }
+];
+let currentStretch = 0;
+
+const progressing = () => {
+
+}
 const onRangeChange = (input) => {
     const val = input.value;
     const string = input.id.split("-")[1];
@@ -46,19 +88,16 @@ const onRangeChange = (input) => {
     target.innerHTML = val;
 
     const comment = comments[string][Number(val)];
-    console.log(comment);
     if (!comment) return;
 
     const tooltip = document.querySelector(`#vital-${string}-tooltip`);
     tooltip.innerHTML = comment;
+
+    tooltip.classList.remove("hidden");
+    setTimeout(() => {
+        tooltip.classList.add("hidden");
+    }, 2000);
 }
-const getVideoData = () => {
-    const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = () => {
-        if (xhttp.readyState !== 4 || xhttp.status !== 200) return;
-        const arr = JSON.parse(xhttp.responseText).slice(0, (type === "community") ? 6 : 3);
-        injectHtml(type, arr);
-    }
-    xhttp.open("GET", ``, true);
-    xhttp.send();
+const init = () => {
+
 }
